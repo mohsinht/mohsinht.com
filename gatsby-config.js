13728e95db979c0ed7351ee75d78942536931472
Mohsin Hayat <mohsinhayat104@gmail.com>
2021-04-21 05:12:26 +0500

    fix gatsby configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: 'Mohsin Hayat'
+    title: 'Mohsin Hayat',
+    siteUrl: 'https://dev.mohsinht.com'
   },
   plugins: [
     'gatsby-plugin-ts',
     'gatsby-plugin-sass',
-    // 'gatsby-plugin-image',
+    'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
-    // 'gatsby-plugin-sitemap',
-    // {
-    //   resolve: 'gatsby-plugin-manifest',
-    //   options: {
-    //     icon: 'src/images/icon.png'
-    //   }
-    // },
-    // 'gatsby-transformer-remark',
-    // 'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png'
+      }
+    },
+    'gatsby-transformer-remark',
+    'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-source-filesystem',
